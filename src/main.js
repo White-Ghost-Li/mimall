dbcff9f6083fb077669706666ca9b15c30564e0f
“White-Ghost-Li” <872023908@qq.com>
2020-10-12 01:22:49 +0800

    three days commit

--- src/main.js
+++ src/main.js
@@ -2,8 +2,22 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import router from './router'
+// 错误拦截
+axios.interceptors.response.use((response) => {
+  let res = response.data
+  if (res.status === 0) {
+    return res.data
+  } else if (response.status === 10) {
+    window.location.href = '/#/login'
+  } else {
+    alert(res.msg)
+  }
+})
 
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
